4bea09366a25520d660b2ba6a39831879a3ddf91
Dhiman Seal <furyx.ds@gmail.com>
2021-10-19 21:20:14 +0530

    Init commit with Assignment 1, 2 done.

--- pages/assignments/assignment1/script/form_action.php
+++ pages/assignments/assignment1/script/form_action.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>CS-481 Assignment 2 (18th October, 2021)</title>
+    <title>CS-481 Assignment 1 (18th October, 2021)</title>
     <link rel="stylesheet" href="../styles.css">
     <link rel="stylesheet" href="../../../common_styles.css">
 </head>
@@ -11,11 +11,15 @@
     <div class="center-card">
         <div class="card">
             <div class="card-header">
-                <h1><u>Information Extracted from Scholar ID: <?php echo $_GET["scholar-id"]; ?></u></h1>
+                <h1><u>Multiplication Table of <?php echo $_GET["number"] . " (" . ($_GET["rows"] ?: 10) . " rows)"; ?></u></h1>
                 <h2><u>Built by - Dhiman Seal (1814071)</u></h2>
             </div>
             <div class="card-body" id="multiplication-table">
-                <?php include "logic.php" ?>
+                <?php
+                for ($i = 1; $i <= ($_GET["rows"] ?: 10); $i++) {
+                    echo $_GET["number"] . "&emsp;*&emsp;" . $i . "&emsp;=&emsp;" . $_GET["number"] * $i . "<br>";
+                }
+                ?>
                 <input class="back-button" type="button" value="Try Again" onclick="window.location.href='../index.html';">
             </div>
         </div>
---
+++ pages/assignments/assignment2/script/logic.php
@@ -0,0 +1,74 @@
+<?php
+// For convenience.
+$h3EndTag = "</h3>";
+$input = $_GET["scholar-id"];
+// Convert $input to string
+$input = (string)$input;
+// If input does not have length 7, then it is not a valid scholar id
+if (strlen($input) != 7) {
+    echo "<h3>Invalid Scholar ID</h3>";
+    echo "<h3>Please enter a valid Scholar ID</h3>";
+} else {
+    // If input has length 7, then it is a valid scholar id
+    // Extract the first 2 digits of the scholar id
+    $first_two_digits = substr($input, 0, 2);
+    echo "<h3>The Student was admitted in: 20" . $first_two_digits . $h3EndTag;
+    // Extract the second 2 digits of the scholar id
+    $second_two_digits = substr($input, 2, 2);
+    if ($second_two_digits[0] == "1") {
+        $branch = "";
+        switch ($second_two_digits[1]) {
+            case "1":
+                $branch = "Civil Engineering";
+                break;
+            case "2":
+                $branch = "Mechanical Engineering";
+                break;
+            case "3":
+                $branch = "Electrical Engineering";
+                break;
+            case "4":
+                $branch = "Electronics and Communication Engineering";
+                break;
+            case "5":
+                $branch = "Computer Science and Engineering";
+                break;
+            case "6":
+                $branch = "Electronics and Instrumentation Engineering";
+                break;
+            default:
+                $branch = "Invalid Branch";
+                break;
+        }
+        echo "<h3>The Student belongs to B. Tech. in " . $branch . $h3EndTag;
+    } else if ($second_two_digits[0] == "2") {
+        switch ($second_two_digits[1]) {
+            case "1":
+                $branch = "Civil Engineering";
+                break;
+            case "2":
+                $branch = "Computer Science and Engineering";
+                break;
+            case "3":
+                $branch = "Electrical Engineering";
+                break;
+            case "4":
+                $branch = "Electronics and Communication Engineering";
+                break;
+            case "5":
+                $branch = "Electronics and Instrumentation Engineering";
+                break;
+            case "6":
+                $branch = "Mechanical Engineering";
+                break;
+            default:
+                $branch = "Invalid Branch";
+                break;
+        }
+        echo "<h3>The Student belongs to M. Tech. in " . $branch . $h3EndTag;
+    } else if ($second_two_digits[0] == "3") {
+        echo "<h3>The Student belongs to PhD";
+    } else {
+        echo "<h3>Unable to deduce what course the student is enrolled in.</h3>";
+    }
+}
