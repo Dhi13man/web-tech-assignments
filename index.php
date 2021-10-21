68bfcb200baa13e478eac5ea398607a9b7041657
Dhiman Seal <furyx.ds@gmail.com>
2021-10-21 20:57:47 +0530

    For dynamic site hosting

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hello</title>
+    <meta http-equiv="Refresh" content="0; URL=pages\landing\landing_page.html">
+</head>
+
+<body>
+    Testing stuff out.
+</body>
+
+</html>
