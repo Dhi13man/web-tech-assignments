9f681c038ec158bd79aad8b44a4aacc6cae45939
Dhiman Seal <furyx.ds@gmail.com>
2021-10-21 21:02:25 +0530

    for dynamic deployment

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Hello</title>
-    <meta http-equiv="Refresh" content="0; URL=pages\landing\landing_page.html">
-</head>
-
-<body>
-    Testing stuff out.
-</body>
-
-</html>
+<?php include_once("index.html"); ?>
