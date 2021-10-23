537b65ab44b668de42d33820593cdd5894bca3bb
Dhiman Seal <furyx.ds@gmail.com>
2021-10-23 12:30:22 +0530

    WWHY DID I HAVE A EXTRA DIV TAG LOL

--- pages/assignments/assignment1/script/form_action.php
+++ pages/assignments/assignment1/script/form_action.php
@@ -9,24 +9,23 @@
 
 <body>
     <div class="center-card">
-        <div class="card">
-            <div class="card-header">
-                <h1><u>Multiplication Table of <?php echo $_GET["number"] . " (" . ($_GET["rows"] ?: 10) . " rows)"; ?></u></h1>
-                <h2><u>Built by - Dhiman Seal (1814071)</u></h2>
-            </div>
-            <div class="card-body" id="multiplication-table">
-                <?php
-                if ($_GET["rows"] <= 0) {
-                    echo "<h3>Please enter a positive number of rows.</h3>";
-                } else {
-                    for ($i = 1; $i <= ($_GET["rows"] ?: 10); $i++) {
-                        echo $_GET["number"] . "&emsp;*&emsp;" . $i . "&emsp;=&emsp;" . $_GET["number"] * $i . "<br>";
-                    }
-                }
-                ?>
-                <input class="back-button" type="button" value="Try Again" onclick="window.location.href='../index.html';">
-            </div>
+        <div class="card-header">
+            <h1><u>Multiplication Table of <?php echo $_GET["number"] . " (" . ($_GET["rows"] ?: 10) . " rows)"; ?></u></h1>
+            <h2><u>Built by - Dhiman Seal (1814071)</u></h2>
         </div>
+        <div class="card-body" id="multiplication-table">
+            <?php
+            if ($_GET["rows"] <= 0) {
+                echo "<h3>Please enter a positive number of rows.</h3>";
+            } else {
+                for ($i = 1; $i <= ($_GET["rows"] ?: 10); $i++) {
+                    echo $_GET["number"] . "&emsp;*&emsp;" . $i . "&emsp;=&emsp;" . $_GET["number"] * $i . "<br>";
+                }
+            }
+            ?>
+            <input class="back-button" type="button" value="Try Again" onclick="window.location.href='../index.html';">
+        </div>
+    </div>
 </body>
 
 </html>
--- pages/assignments/assignment2/script/form_action.php
+++ pages/assignments/assignment2/script/form_action.php
@@ -9,16 +9,15 @@
 
 <body>
     <div class="center-card">
-        <div class="card">
-            <div class="card-header">
-                <h1><u>Information Extracted from Scholar ID: <?php echo $_GET["scholar-id"]; ?></u></h1>
-                <h2><u>Built by - Dhiman Seal (1814071)</u></h2>
-            </div>
-            <div class="card-body">
-                <?php include "logic.php" ?>
-                <input class="back-button" type="button" value="Try Again" onclick="window.location.href='../index.html';">
-            </div>
+        <div class="card-header">
+            <h1><u>Information Extracted from Scholar ID: <?php echo $_GET["scholar-id"]; ?></u></h1>
+            <h2><u>Built by - Dhiman Seal (1814071)</u></h2>
         </div>
+        <div class="card-body">
+            <?php include "logic.php" ?>
+            <input class="back-button" type="button" value="Try Again" onclick="window.location.href='../index.html';">
+        </div>
+    </div>
 </body>
 
 </html>
